82eb1c7e093830e454bb4efc864ae0dbc5a681be
Minhajulislam0 <mdmishu98730@gmail.com>
2023-01-25 20:02:43 +0600

    Initital Navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Feed from "./components/Feed";
+import VideoDetail from "./components/VideoDetail";
+import ChannalDetail from "./components/ChannalDetail";
+import SearchFeed from "./components/SearchFeed";
 
 export default function App() {
-  return <div>hi there</div>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channal/:id" element={<ChannalDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 }
---
+++ src/components/ChannalDetail.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ChannalDetail() {
+  return <div>ChannalDetail</div>;
+}
